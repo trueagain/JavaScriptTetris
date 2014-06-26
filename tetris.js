97923da6c610668d4437f709031012779985cd51
unknown <truewhile@gmail.com>
2014-06-26 12:12:11 +0400

    all pieces added

--- tetris.js
+++ tetris.js
@@ -14,14 +14,75 @@ function tetrisMainFunction(){
 		result.I.PARTS_POSITION_3 = [[1, 0], [1, 1], [1, 2], [1, 3]];
 	    result.I.PARTS_POSITION_4 = [[0, 1], [1, 1], [2, 1], [3, 1]];
 	    result.I.partsPosition = result.I.PARTS_POSITION_1;
+	    result.J = new Object;
+	    result.J.PARTS_POSITION_1 = [[0, 0], [1, 0], [1, 1], [1, 2]];
+	    result.J.PARTS_POSITION_2 = [[0, 1], [0, 2], [1, 1], [2, 1]];
+		result.J.PARTS_POSITION_3 = [[1, 0], [1, 1], [1, 2], [2, 2]];
+	    result.J.PARTS_POSITION_4 = [[0, 1], [1, 1], [2, 0], [2, 1]];
+	    result.J.partsPosition = result.J.PARTS_POSITION_1;
+	    result.L = new Object;
+	    result.L.PARTS_POSITION_1 = [[0, 2], [1, 0], [1, 1], [1, 2]];
+	    result.L.PARTS_POSITION_2 = [[0, 1], [1, 1], [2, 1], [2, 2]];
+		result.L.PARTS_POSITION_3 = [[1, 0], [1, 1], [1, 2], [2, 0]];
+	    result.L.PARTS_POSITION_4 = [[0, 0], [0, 1], [1, 1], [2, 1]];
+	    result.L.partsPosition = result.L.PARTS_POSITION_1;
+	    result.O = new Object;
+	    result.O.PARTS_POSITION_1 = [[0, 0], [0, 1], [1, 0], [1, 1]];
+	    result.O.PARTS_POSITION_2 = [[0, 0], [0, 1], [1, 0], [1, 1]];
+		result.O.PARTS_POSITION_3 = [[0, 0], [0, 1], [1, 0], [1, 1]];
+	    result.O.PARTS_POSITION_4 = [[0, 0], [0, 1], [1, 0], [1, 1]];
+	    result.O.partsPosition = result.O.PARTS_POSITION_1;
+	    result.S = new Object;
+	    result.S.PARTS_POSITION_1 = [[1, 1], [1, 2], [2, 0], [2, 1]];
+	    result.S.PARTS_POSITION_2 = [[0, 1], [1, 1], [1, 2], [2, 2]];
+		result.S.PARTS_POSITION_3 = [[1, 1], [1, 2], [2, 0], [2, 1]];
+	    result.S.PARTS_POSITION_4 = [[0, 1], [1, 1], [1, 2], [2, 2]];
+	    result.S.partsPosition = result.S.PARTS_POSITION_1;
+	    result.T = new Object;
+	    result.T.PARTS_POSITION_1 = [[0, 1], [1, 0], [1, 1], [1, 2]];
+	    result.T.PARTS_POSITION_2 = [[0, 1], [1, 1], [1, 2], [2, 1]];
+		result.T.PARTS_POSITION_3 = [[1, 0], [1, 1], [1, 2], [2, 1]];
+	    result.T.PARTS_POSITION_4 = [[0, 1], [1, 0], [1, 1], [2, 1]];
+	    result.T.partsPosition = result.T.PARTS_POSITION_1;
+	    result.Z = new Object;
+	    result.Z.PARTS_POSITION_1 = [[1, 0], [1, 1], [2, 1], [2, 2]];
+	    result.Z.PARTS_POSITION_2 = [[0, 1], [1, 0], [1, 1], [2, 0]];
+		result.Z.PARTS_POSITION_3 = [[1, 0], [1, 1], [2, 1], [2, 2]];
+	    result.Z.PARTS_POSITION_4 = [[0, 1], [1, 0], [1, 1], [2, 0]];
+	    result.Z.partsPosition = result.Z.PARTS_POSITION_1;
 	    return result;
 	}
 	tetrisModel.allPieces = tetrisModel.createPieces();
 	tetrisModel.chooseNewActivePiece = function(){
-		var result = this.allPieces.I;
-		result.position = [3, 5];
-		result.partsPosition = result.PARTS_POSITION_1;
-		return result;
+		var randPieceNum = Math.random() * 7;
+		var randPiece;
+		if(randPieceNum <= 1){
+			randPiece = this.allPieces.I;
+		} else if(randPieceNum <= 2){
+			randPiece = this.allPieces.J;
+		} else if(randPieceNum <= 3){
+			randPiece = this.allPieces.L;
+		} else if(randPieceNum <= 4){
+			randPiece = this.allPieces.O;
+		} else if(randPieceNum <= 5){
+			randPiece = this.allPieces.S;
+		} else if(randPieceNum <= 6){
+			randPiece = this.allPieces.T;
+		} else {
+			randPiece = this.allPieces.Z;
+		}
+		var randPosNum = Math.random() * 4;
+		if(randPosNum <= 1){
+			randPiece.partsPosition = randPiece.PARTS_POSITION_1;
+		} else if(randPosNum <= 2){
+			randPiece.partsPosition = randPiece.PARTS_POSITION_2;
+		} else if(randPosNum <= 3){
+			randPiece.partsPosition = randPiece.PARTS_POSITION_3;
+		} else{
+			randPiece.partsPosition = randPiece.PARTS_POSITION_4;
+		}
+		randPiece.position = [-1, 5];
+		return randPiece;
 	}
 	tetrisModel.activePiece = tetrisModel.chooseNewActivePiece();
 	tetrisModel.setNewActivePiece = function(){
